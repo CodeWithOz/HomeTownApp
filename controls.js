6c2264908e4f6b742a8882276d706d0f4bdfb922
Uche Ozoemena <ucheozoemena@gmail.com>
2018-07-15 06:23:17 +0100

    Use Udacity final project version
    This commit updates the source code to that given by Udacity as
    the starting point for the final set of updates to the web app.
    I would rather work on that and have more in common with the
    videos than continue with mine and potentially miss out on
    styling that I have not yet encountered.

--- controls.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-
-// toggle navbar when hamburger is clicked
-hamburger.addEventListener('click', event => {
-  event.preventDefault();
-  const navList = document.querySelector('.nav__list');
-  navList.classList.toggle('open');
-});
